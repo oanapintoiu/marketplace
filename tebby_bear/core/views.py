8a32df03e6c2e8ef3332971185647829055f75eb
Oana Pintoiu <oana.pintoiu@yahoo.com>
2024-09-06 21:58:11 +0100

    created sigup page, no form

--- tebby_bear/core/views.py
+++ tebby_bear/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from item.models import Category, Item
+from .forms import SignupForm
 
 def index(request): 
     items = Item.objects.filter(is_sold=False) [0:6]
@@ -12,5 +13,11 @@ def index(request):
 
 def contact(request): 
     return render(request, 'core/contact.html')
+
+def signup(request):
+    form = SignupForm()
+    return render(request, 'core/signup.html', {
+        'form': form
+    })
 
 # Create your views here.
