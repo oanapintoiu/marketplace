36db40536e2f07b5bbf6976f28f7879f8ccd4779
Oana Pintoiu <oana.pintoiu@yahoo.com>
2024-09-08 18:22:27 +0100

    added post for signup form with redirection to login if signup successful

--- tebby_bear/core/views.py
+++ tebby_bear/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from item.models import Category, Item
 from .forms import SignupForm
 
@@ -22,6 +22,13 @@ def contact(request):
 
 
 def signup(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("/login")
+    else:
+        form = SignupForm()
     form = SignupForm()
     return render(request, "core/signup.html", {"form": form})
 
