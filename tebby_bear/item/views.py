31698a376e31d15fccae0fe72a5aecc65791985a
Oana Pintoiu <oana.pintoiu@yahoo.com>
2024-09-11 18:41:12 +0100

    user can now search by description, i.e. description to contian word that is being used in the search bar

--- tebby_bear/item/views.py
+++ tebby_bear/item/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .forms import NewItemForm, EditItemForm
@@ -10,7 +11,7 @@ def items(request):
     items = Item.objects.filter(is_sold=False)
 
     if query:
-        items = items.filter(name__icontains=query)
+        items = items.filter(Q(name__icontains=query) | Q(description__icontains=query))
 
     return render(
         request,
